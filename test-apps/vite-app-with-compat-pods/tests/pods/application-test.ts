801ee3bc169da539c2c7233271485f6f269abd3a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-03 10:05:41 -0400

    Update test

--- test-apps/vite-app-with-compat-pods/tests/pods/application-test.ts
+++ test-apps/vite-app-with-compat-pods/tests/pods/application-test.ts
@@ -18,8 +18,6 @@ module('[In App] pod:application (hbs)', function (hooks) {
           'vite-app-with-compat-pods/pods/application'
         )
       );
-    assert
-      .dom('p')
-      .hasStyle({ textDecoration: 'underline solid rgb(0, 0, 0)' });
+    assert.dom('p').hasStyle({ textDecoration: 'underline rgb(0, 0, 0)' });
   });
 });
