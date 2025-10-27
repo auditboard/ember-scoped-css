c90fa5ef88dcf4d1d652c45f3dd13f8a6ce35102
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-27 17:45:19 -0400

    Debug windows

--- ember-scoped-css/src/lib/path/hash-from-absolute-path.test.ts
+++ ember-scoped-css/src/lib/path/hash-from-absolute-path.test.ts
@@ -3,14 +3,13 @@ import path from 'node:path';
 import { describe, expect, it } from 'vitest';
 
 import { hashFromAbsolutePath } from './hash-from-absolute-path.js';
-import { forcePosix } from './utils.js';
 import { paths } from './utils.paths.test.js';
 
 describe('hashFromAbsolutePath', () => {
   describe(`the module: "embroider-app/templates/application"`, () => {
     it('works with direct path', () => {
       let filePath = path.join(paths.viteApp, 'src/components/third');
-      let postfix = hashFromAbsolutePath(forcePosix(filePath));
+      let postfix = hashFromAbsolutePath(filePath);
 
       expect(postfix).to.equal('e4b5bd4dc');
     });
