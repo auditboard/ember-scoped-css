b43f3691d46f1d2f736cef7d53c1e24128d31d04
gossi <gossi@users.noreply.github.com>
2025-11-01 12:43:49 +0100

    Switch from `esbuild` to `tsdown` for watch mode

--- ember-scoped-css/src/runtime/index.ts
+++ ember-scoped-css/src/runtime/index.ts
@@ -2,7 +2,7 @@
  * Appends a suffix to a class name during build time.
  * This function is not available at runtime and is removed.
  *
- * @param {string} className the class, defined within the co-located CSS to have a suffix appended to it during build time.
+ * @param className the class, defined within the co-located CSS to have a suffix appended to it during build time.
  */
 export function scopedClass(className: string): string {
   throw new Error(
---
+++ ember-scoped-css/tsdown.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig([
+  {
+    entry: [
+      'src/build/index.js',
+      'src/build/babel-plugin.js',
+      'src/build/template-plugin.js',
+    ],
+    outDir: 'dist/cjs',
+    format: 'cjs',
+    sourcemap: 'inline',
+    dts: false,
+
+    define: {
+      'import.meta.vitest': 'false',
+    },
+  },
+  {
+    entry: ['src/runtime/test-support.ts', 'src/runtime/index.ts'],
+    outDir: 'dist/runtime',
+    sourcemap: true
+  },
+]);
