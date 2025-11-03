12c1c1440169245f15d838baae85ac14fd0ef045
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-03 08:39:13 -0500

    Split colocated vs inline handling to separate (internal) plugins. Resolves #361

--- ember-scoped-css/src/build/scoped-css-unplugin.js
+++ ember-scoped-css/src/build/scoped-css-unplugin.js
@@ -20,111 +20,97 @@ const ORIGIN = {
 /**
  * The plugin that handles CSS requests for `<style>` elements and transforms
  * for existing files
+ *
+ * vite: CSS files are resolved by vite. We use their resolver to also get
+ *       HMR. That is, for all non-physical CSS files, we extend vite by our
+ *       resolver and also can enrich metadata to it (for better debugging)
  */
 export default createUnplugin((options = {}) => {
-  return {
-    name: 'ember-scoped-css-unplugin',
+  return [
+    {
+      name: 'ember-scoped-css:colocated',
+      resolveId(id, importer) {
+        // handles: some-file.css?scoped=[postfix]
+        // this is only run in rollup, vite handles it differently
+        if (isSeparateCSSFileRequest(id)) {
+          const parsed = decodeSeparateCSSFileRequest(id);
 
-    // vite: CSS files are resolved by vite. We use their resolver to also get
-    // HMR. That is, for all non-physical CSS files, we extend vite by our
-    // resolver and also can enrich metadata to it (for better debugging)
-    resolveId(id, importer) {
-      // handles: some-file.css?scoped=[postfix]
-      // this is only run in rollup, vite handles it differently
-      if (isSeparateCSSFileRequest(id)) {
-        const parsed = decodeSeparateCSSFileRequest(id);
-
-        // we change the id and drop the query string, this makes it so rollup
-        // understands it at CSS and can load it appropriately
-        return {
-          id: path.resolve(
-            path.dirname(importer),
-            path.basename(parsed.fileName),
-          ),
-          meta: {
-            'scoped-css': {
-              postfix: parsed.postfix,
-              origin: ORIGIN.File,
-              fileName: parsed.fileName,
+          // we change the id and drop the query string, this makes it so rollup
+          // understands it at CSS and can load it appropriately
+          return {
+            id: path.resolve(
+              path.dirname(importer),
+              path.basename(parsed.fileName),
+            ),
+            meta: {
+              'scoped-css': {
+                postfix: parsed.postfix,
+                origin: ORIGIN.File,
+                fileName: parsed.fileName,
+              },
             },
-          },
-        };
-      }
+          };
+        }
+      },
+      load(id) {
+        // this is only for vite
+        if (isSeparateCSSFileRequest(id)) {
+          const parsed = decodeSeparateCSSFileRequest(id);
 
-      if (isStyleElementCSSRequest(id)) {
-        const parsed = decodeStyleElementCSSRequest(id);
+          return readFileSync(parsed.fileName, 'utf-8');
+        }
+      },
+      transform(code, id) {
+        if (isSeparateCSSFileRequest(id)) {
+          const meta = this.getModuleInfo(id)?.meta?.['scoped-css'];
+          const parsed = decodeSeparateCSSFileRequest(id);
+          const filePath = meta?.fileName ?? path.relative(cwd(), id);
 
-        return {
-          id: path.resolve(
-            path.dirname(importer),
-            `${path.basename(importer, path.extname(importer))}-${parsed.hash}.css`,
-          ),
-          meta: {
-            'scoped-css': {
-              css: parsed.css,
-              postfix: parsed.postfix,
-              origin: ORIGIN.Style,
-              fileName: path.relative(cwd(), importer),
+          return rewriteCss(code, parsed.postfix, filePath, options.layerName);
+        }
+      },
+    },
+    {
+      name: 'ember-scoped-css:inline',
+      resolveId(id, importer) {
+        if (isStyleElementCSSRequest(id)) {
+          const parsed = decodeStyleElementCSSRequest(id);
+
+          return {
+            id: path.resolve(
+              path.dirname(importer),
+              `${path.basename(importer, path.extname(importer))}-${parsed.hash}.css`,
+            ),
+            meta: {
+              'scoped-css': {
+                css: parsed.css,
+                postfix: parsed.postfix,
+                origin: ORIGIN.Style,
+                fileName: path.relative(cwd(), importer),
+              },
             },
-          },
-        };
-      }
+          };
+        }
+      },
+      load(id) {
+        const meta = this.getModuleInfo(id)?.meta?.['scoped-css'];
+
+        if (meta) {
+          return meta.css;
+        }
+      },
+      transform(code, id) {
+        const meta = this.getModuleInfo(id)?.meta?.['scoped-css'];
+
+        if (meta) {
+          return rewriteCss(
+            code,
+            meta.postfix,
+            `${meta.origin === ORIGIN.Style ? '<inline>/' : ''}${meta.fileName}`,
+            options.layerName,
+          );
+        }
+      },
     },
-
-    load(id) {
-      // this is only for vite
-      if (isSeparateCSSFileRequest(id)) {
-        const parsed = decodeSeparateCSSFileRequest(id);
-
-        return readFileSync(parsed.fileName, 'utf-8');
-      }
-
-      // this is for the `<style>` tag related loading
-      const meta = this.getModuleInfo(id)?.meta?.['scoped-css'];
-
-      if (meta) {
-        return meta.css;
-      }
-    },
-
-    transform(code, id) {
-      // rollup: transform separate CSS file
-      // vite: transform <style> CSS
-      const meta = this.getModuleInfo(id)?.meta?.['scoped-css'];
-
-      if (meta) {
-        return rewriteCss(
-          code,
-          meta.postfix,
-          `${meta.origin === ORIGIN.Style ? '<inline>/' : ''}${meta.fileName}`,
-          options.layerName,
-        );
-      }
-
-      // vite: transform separate CSS file
-      if (isSeparateCSSFileRequest(id)) {
-        const parsed = decodeSeparateCSSFileRequest(id);
-        const filePath = meta?.fileName ?? path.relative(cwd(), id);
-
-        return rewriteCss(code, parsed.postfix, filePath, options.layerName);
-      }
-    },
-
-    // this below is actually the format for transforms, so rust based runtimes
-    // operate more performant - but that didn't work by the time I wrote this
-    // come back later and test again
-    // transform: {
-    //   filter: {
-    //     id: /\.css\?scoped=([a-zA-Z0-9]+)$/
-    //   },
-    //   handler(code, id) {
-    //     const [_, qs] = id.split('?');
-    //     const search = new URLSearchParams(qs);
-
-    //     const filePath = path.relative(id, cwd());
-
-    //     return rewriteCss(code, search.get('scoped'), filePath, options.layerName);
-    //   }
-    // },
-  };
+  ];
 });
