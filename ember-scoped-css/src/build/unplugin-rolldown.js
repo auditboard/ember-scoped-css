c2a215bc882d3dbf5ca452b30778cd8405ea2b34
gossi <gossi@users.noreply.github.com>
2025-11-08 22:09:52 +0100

    With docs and credits to its origin

--- ember-scoped-css/src/build/unplugin-rolldown.js
+++ ember-scoped-css/src/build/unplugin-rolldown.js
@@ -79,6 +79,8 @@ const extractStyleImports = (code) => {
 };
 
 /**
+ * This is mostly based on: https://github.com/bosh-code/tsdown-plugin-inject-css
+ * With a little addition to make it work for the virtual CSS files of this project
  */
 export function rolldown() {
   // Track style imports per module
