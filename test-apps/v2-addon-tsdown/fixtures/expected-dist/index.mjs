d00e2c446d385b304ae5a96da4ef2649dbabd812
gossi <gossi@users.noreply.github.com>
2025-11-08 22:12:39 +0100

    fix fixtures test

--- test-apps/v2-addon-tsdown/fixtures/expected-dist/index.mjs
+++ test-apps/v2-addon-tsdown/fixtures/expected-dist/index.mjs
@@ -4,10 +4,6 @@ import templateOnly from "@ember/component/template-only";
 import { concat } from "@ember/helper";
 
 import './index.css';
-//#region src/components/time.gts
-var time_default = setComponentTemplate(precompileTemplate("\n  <h1 class=\"e227860bf\">Time</h1>\n", { strictMode: true }), templateOnly());
-
-//#endregion
 //#region src/components/alert.gjs
 var alert_default = setComponentTemplate(precompileTemplate("\n  <div class=\"ef9808f67\">\n    <h3 class=\"header_ef9808f67\">\n      {{@title}}\n    </h3>\n    <p class=\"message_ef9808f67 {{@some}}\">\n      {{@message}}\n    </p>\n  </div>\n", { strictMode: true }), templateOnly());
 
@@ -27,6 +23,10 @@ var header_default = setComponentTemplate(precompileTemplate("\n  <div class=\"a
 var inline_default = setComponentTemplate(precompileTemplate("\n  <h1 class=\"eb86dbfce\">Time</h1>\n\n  \n  <style>\n    @layer firstLayer {\n      h1 { color: rgb(0, 200, 0); }\n    }\n  </style>\n", { strictMode: true }), templateOnly());
 const SecondScoped = setComponentTemplate(precompileTemplate("\n  <h6 class=\"eb86dbfce\">Second Scoped</h6>\n\n  \n", { strictMode: true }), templateOnly());
 const ActuallyInline = setComponentTemplate(precompileTemplate("\n  <h6 class=\"eb86dbfce\">Actually Inline</h6>\n\n  <style scoped inline>/* src/components/inline.css */\n\n    h6.eb86dbfce { color: rgb(200, 0, 0); }\n</style>\n", { strictMode: true }), templateOnly());
+
+//#endregion
+//#region src/components/time.gts
+var time_default = setComponentTemplate(precompileTemplate("\n  <h1 class=\"e227860bf\">Time</h1>\n", { strictMode: true }), templateOnly());
 
 //#endregion
 export { ActuallyInline, alert_default as Alert, footer_default as Footer, header_default as Header, inline_default as Inline, SecondScoped, time_default as Time };
