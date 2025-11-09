5863e26163363851eb230820f98fa4ed2f6cf157
gossi <gossi@users.noreply.github.com>
2025-11-09 03:13:37 +0100

    run prettier

--- test-apps/v2-addon-tsdown/src/index.js
+++ test-apps/v2-addon-tsdown/src/index.js
@@ -1,7 +1,7 @@
 export { default as Alert } from './components/alert.gjs';
 export { default as Footer } from './components/footer.gjs';
 export { default as Header } from './components/header.gjs';
-export {default as Body} from './card.gjs';
+export { default as Body } from './card.gjs';
 export {
   ActuallyInline,
   default as Inline,
