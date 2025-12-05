8fe0ea84ed2bb4e291df9bfd881c1928ee652804
Liam Potter <radioactivity@gmail.com>
2025-12-05 18:57:30 +0000

    fix typo

--- ember-scoped-css/src/lib/path/utils.js
+++ ember-scoped-css/src/lib/path/utils.js
@@ -259,7 +259,7 @@ export function isRelevantFile(fileName, { additionalRoots, cwd }) {
   let roots = [
     leadingSlashPath.componentsDir,
     leadingSlashPath.templatesDir,
-    leadingslashPath.routesDir,
+    leadingSlashPath.routesDir,
     ...(additionalRoots || []),
   ];
 
