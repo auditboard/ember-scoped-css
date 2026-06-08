73dc222106a187a9dd330bcc31eb973e0c4cbb91
Ryan Mark <rgmark@gmail.com>
2026-06-08 15:55:57 -0700

    refactor: throw on unknown processor type in resolveProcessor

--- ember-scoped-css/src/lib/css/processors/index.js
+++ ember-scoped-css/src/lib/css/processors/index.js
@@ -9,8 +9,20 @@ const processors = {
  * @param {{ type?: 'postcss' | 'lightningcss' }} [options]
  */
 export function resolveProcessor(lang, options = {}) {
-  if (options.type) return processors[options.type];
+  if (options.type) {
+    const processor = processors[options.type];
 
-  // Default during refactor is postcss; flipped to lightningcss in a later task.
+    if (!processor) {
+      throw new Error(
+        `[ember-scoped-css] Unknown CSS processor type: "${options.type}". ` +
+          `Valid types: ${Object.keys(processors).join(', ')}.`,
+      );
+    }
+
+    return processor;
+  }
+
+  // `lang`-based routing (e.g. scss -> postcss) is added in a later task.
+  // For now everything resolves to postcss.
   return processors.postcss;
 }
